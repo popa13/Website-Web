069e1ccfc281b134570cd63343bcf45aa4ba0205
Pierre Laptop <pierre_olivierp@hotmail.ca>
2022-06-17 11:59:03 -1000

    Added annotated notes from section 1-2, section 1-3. Added python files for sections 1-1, 1-2 and 1-3

--- Courses-Website/MATH-307/documents/4-Python/Section1-1.py
+++ Courses-Website/MATH-307/documents/4-Python/Section1-1.py
@@ -1,8 +1,10 @@
 import numpy
+from numpy import mgrid, cos, pi, sin
 import sympy
 
 from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
+from mayavi import mlab
 
 
 def interchangeEq(indexEq1, indexEq2, matrix):
@@ -41,10 +43,11 @@ while not stop:
     ##### Example 3:
     #########################
     print("________ Example 3 _________")
-    printStep = int(input("Which step do you want to print? Enter a number?\nChoose between 1, 2, and 3.\n"))
+    printStep = input("Which step do you want to print? Enter a number?\nChoose between the following options\n 1. Print step 1.\n 2. Print steps 1 and 2.\n 3. Print steps 1, 2, and 3\n"
+                          " 4. Plot the solution and the planes.\nEnter q if you want to quit.\n")
 
     #### Step 1
-    if printStep == 1:
+    if printStep != 'q' and int(printStep) == 1:
         # Augmented matrix
         m = sympy.Matrix([[2, 3, -1, 3], [-1, -1, 3, 0], [1, 2, 2, 3], [0, 1, 5, 3]])
 
@@ -56,47 +59,19 @@ while not stop:
         interchangeByCombination(3, 1, -2, 1, m)
         printMatrix(m)
 
-        ### Plot the planes obtained after this step
-        x = numpy.linspace(-10, 10, 20)
-        y = numpy.linspace(-10, 10, 20)
-        ones = numpy.ones(100)
-        xGrid, yGrid = numpy.meshgrid(y, x)
-        onesGrid = numpy.meshgrid(ones, ones)
-        # First plane
-        plane1 = m.row(0)[3] / m.row(0)[2] - (m.row(0)[0] / m.row(0)[2]) * xGrid - (m.row(0)[1] / m.row(0)[2]) * yGrid
-
-        # Second plane
-        plane2 = m.row(1)[3] / m.row(1)[2] - (m.row(1)[0] / m.row(1)[2]) * xGrid - (m.row(1)[1] / m.row(1)[2]) * yGrid
-
-        # Third Plane
-        plane3 = m.row(2)[3] / m.row(2)[2] - (m.row(2)[0] / m.row(2)[2]) * xGrid - (m.row(2)[1] / m.row(2)[2]) * yGrid
-
-        # Fourth Plane
-        plane4 = m.row(3)[3] / m.row(3)[2] - (m.row(3)[0] / m.row(3)[2]) * xGrid - (m.row(3)[1] / m.row(3)[2]) * yGrid
-
-        fig = plt.figure()
-        ax = plt.axes(projection='3d')
-
-        ax.plot_surface(xGrid, yGrid, plane1)
-        ax.plot_surface(xGrid, yGrid, plane2)
-        ax.plot_surface(xGrid, yGrid, plane3)
-        ax.plot_surface(xGrid, yGrid, plane4)
-
-        ax.set_xlim(-10, 10)
-        ax.set_ylim(-10, 10)
-        ax.set_zlim(-40, 40)
-
-        plt.show()
-
     #######
     # Second operations to get rid of the y
-    if printStep == 2:
+    if printStep != 'q' and int(printStep) == 2:
         # Augmented matrix
         m = sympy.Matrix([[2, 3, -1, 3], [-1, -1, 3, 0], [1, 2, 2, 3], [0, 1, 5, 3]])
 
         # First operations to get rid of the x's
+        print("___ Step 1 ___")
+        print("L1 + 2L2 -> L2")
         interchangeByCombination(2, 1, 2, 1, m)
+        print("L1 - 2L3 -> L3")
         interchangeByCombination(3, 1, -2, 1, m)
+        printMatrix(m)
 
         print("___ Step 2 ___")
         print("3L2 - L1 -> L1")
@@ -108,151 +83,84 @@ while not stop:
         printMatrix(m)
         print("Notice the two lines of zeros!")
 
-        ### Plot the planes obtained after this step
-        x = numpy.linspace(-10, 10, 20)
-        y = numpy.linspace(-10, 10, 20)
-        ones = numpy.ones(100)
-        xGrid, yGrid = numpy.meshgrid(y, x)
-        onesGrid = numpy.meshgrid(ones, ones)
-        # First plane
-        plane1 = m.row(0)[3] / m.row(0)[2] - (m.row(0)[0] / m.row(0)[2]) * xGrid - (m.row(0)[1] / m.row(0)[2]) * yGrid
-
-        # Second plane
-        plane2 = m.row(1)[3] / m.row(1)[2] - (m.row(1)[0] / m.row(1)[2]) * xGrid - (m.row(1)[1] / m.row(1)[2]) * yGrid
-
-        # Third Plane
-        plane3 = m.row(2)[3] / m.row(2)[2] - (m.row(2)[0] / m.row(2)[2]) * xGrid - (m.row(2)[1] / m.row(2)[2]) * yGrid
-
-        # Fourth Plane
-        plane4 = m.row(3)[3] / m.row(3)[2] - (m.row(3)[0] / m.row(3)[2]) * xGrid - (m.row(3)[1] / m.row(3)[2]) * yGrid
-
-        fig = plt.figure()
-        ax = plt.axes(projection='3d')
-
-        ax.plot_surface(xGrid, yGrid, plane1)
-        ax.plot_surface(xGrid, yGrid, plane2)
-        ax.plot_surface(xGrid, yGrid, plane3)
-        ax.plot_surface(xGrid, yGrid, plane4)
-
-        ax.set_xlim(-10, 10)
-        ax.set_ylim(-10, 10)
-        ax.set_zlim(-5, 5)
-
-        plt.show()
-
     ##### Step 3
     # Third operations to obtain 1 on the diagonal
-    if printStep == 3:
+    if printStep != 'q' and int(printStep) == 3:
         # Augmented matrix
         m = sympy.Matrix([[2, 3, -1, 3], [-1, -1, 3, 0], [1, 2, 2, 3], [0, 1, 5, 3]])
 
         # First operations to get rid of the x's
+        print("___ Step 1 ___")
+        print("L1 + 2L2 -> L2")
         interchangeByCombination(2, 1, 2, 1, m)
+        print("L1 - 2L3 -> L3")
         interchangeByCombination(3, 1, -2, 1, m)
+        printMatrix(m)
 
-        # Second operations to get rid of the y's
+        print("___ Step 2 ___")
+        print("3L2 - L1 -> L1")
         interchangeByCombination(1, 2, -1, 3, m)
+        print("L2 + L3 -> L3")
         interchangeByCombination(3, 2, 1, 1, m)
+        print("L2 - L4 -> L4")
         interchangeByCombination(4, 2, -1, 1, m)
+        printMatrix(m)
+        print("Notice the two lines of zeros!")
 
         print("___ Step 3 ___")
         print("(-1/2)L1 -> L1")
         multScalar(1, -0.5, m)
         printMatrix(m)
 
-        ### Plot the planes obtained after this step
-        x = numpy.linspace(-10, 10, 20)
-        y = numpy.linspace(-10, 10, 20)
-        ones = numpy.ones(100)
-        xGrid, yGrid = numpy.meshgrid(y, x)
-        onesGrid = numpy.meshgrid(ones, ones)
-        # First plane
-        plane1 = m.row(0)[3] / m.row(0)[2] - (m.row(0)[0] / m.row(0)[2]) * xGrid - (m.row(0)[1] / m.row(0)[2]) * yGrid
-
-        # Second plane
-        plane2 = m.row(1)[3] / m.row(1)[2] - (m.row(1)[0] / m.row(1)[2]) * xGrid - (m.row(1)[1] / m.row(1)[2]) * yGrid
-
-        # Third Plane
-        plane3 = m.row(2)[3] / m.row(2)[2] - (m.row(2)[0] / m.row(2)[2]) * xGrid - (m.row(2)[1] / m.row(2)[2]) * yGrid
-
-        # Fourth Plane
-        plane4 = m.row(3)[3] / m.row(3)[2] - (m.row(3)[0] / m.row(3)[2]) * xGrid - (m.row(3)[1] / m.row(3)[2]) * yGrid
-
-        # Set of solutions
-        z = numpy.linspace(-5, 5, 20)
-        xsol = -3 + 8 * z
-        ysol = 3 - 5 * z
-
-        fig = plt.figure()
-        ax = plt.axes(projection='3d')
-
-        ax.plot_surface(xGrid, yGrid, plane1)
-        ax.plot_surface(xGrid, yGrid, plane2)
-        ax.plot_surface(xGrid, yGrid, plane3)
-        ax.plot_surface(xGrid, yGrid, plane4)
-        ax.plot(xsol, ysol, z, color='black')
-
-        ax.set_xlim(-10, 10)
-        ax.set_ylim(-10, 10)
-        ax.set_zlim(-5, 5)
-
-        plt.show()
-
-    # Print the general solution
-    printSol = int(input("Do you want to see the general solution? Enter 0 for yes and 1 for no.\n"))
-    if printSol == 0:
         print("General solution:")
-        print("""x - 8z = -3
-        y + 5z = 3 """)
-        print("""x = -3 + 8z
-        y = 3 - 5z""")
+        print("""x = -3 + 8z\ny= 3 - 5z""")
         print("The variable z is a free parameter.")
 
-        ##
-        # plot the planes and the solutions
+        print("_______________________________________")
+
+    # Print the general solution
+    if printStep != 'q' and int(printStep) == 4:
+        print("_______________________________________")
+        print("Plotting the solutions and the planes.")
         m = sympy.Matrix([[2, 3, -1, 3], [-1, -1, 3, 0], [1, 2, 2, 3], [0, 1, 5, 3]])
 
-        x = numpy.linspace(-10, 10, 20)
-        y = numpy.linspace(-10, 10, 20)
-        ones = numpy.ones(100)
-        xGrid, yGrid = numpy.meshgrid(y, x)
-        onesGrid = numpy.meshgrid(ones, ones)
+        def plane1(x, y):
+            return 3 / -1.0 - 2 * x / -1.0 - 3 * y / -1.0
 
-        # First plane
-        plane1 = m.row(0)[3] / m.row(0)[2] - (m.row(0)[0] / m.row(0)[2]) * xGrid - (m.row(0)[1] / m.row(0)[2]) * yGrid
 
-        # Second plane
-        plane2 = m.row(1)[3] / m.row(1)[2] - (m.row(1)[0] / m.row(1)[2]) * xGrid - (m.row(1)[1] / m.row(1)[2]) * yGrid
+        def plane2(x, y):
+            return x / 3.0 + y / 3.0
 
-        # Third Plane
-        plane3 = m.row(2)[3] / m.row(2)[2] - (m.row(2)[0] / m.row(2)[2]) * xGrid - (m.row(2)[1] / m.row(2)[2]) * yGrid
 
-        # Fourth Plane
-        plane4 = m.row(3)[3] / m.row(3)[2] - (m.row(3)[0] / m.row(3)[2]) * xGrid - (m.row(3)[1] / m.row(3)[2]) * yGrid
+        def plane3(x, y):
+            return -x / 2.0 - 2 * y / 2.0 + 3.0 / 2
+
+
+        def plane4(x, y):
+            return -y / 5.0 + 3 / 5.0
+
+
+        ### Plot with mayavi
+        dx, dy = 1, 1
+        xGrid, yGrid = mgrid[-10:10:dx, -10:10:dy]
 
         # Set of solutions
         z = numpy.linspace(-5, 5, 20)
         xsol = -3 + 8 * z
         ysol = 3 - 5 * z
 
-        # plot with matplotlib
-        fig = plt.figure()
-        ax = plt.axes(projection='3d')
+        # View it.
+        mlab.surf(xGrid, yGrid, plane1, color=(1, 0, 0))
+        mlab.surf(xGrid, yGrid, plane2, color=(1, 1, 0))
+        mlab.surf(xGrid, yGrid, plane3, color=(1, 1, 1))
+        mlab.surf(xGrid, yGrid, plane4, color=(1, 0, 1))
+        mlab.plot3d(xsol, ysol, z, color=(0, 0, 0), line_width=00.0)
+        mlab.show()
 
-        ax.plot_surface(xGrid, yGrid, plane1)
-        ax.plot_surface(xGrid, yGrid, plane2)
-        ax.plot_surface(xGrid, yGrid, plane3)
-        ax.plot_surface(xGrid, yGrid, plane4)
-        ax.plot3D(xsol, ysol, z, 'black')
-        ax.set_xlim(-10, 10)
-        ax.set_ylim(-10, 10)
-        ax.set_zlim(-40, 20)
+        print("_______________________________________")
 
-        plt.savefig("example3.png")
-        plt.show()
-
-    choice = input("Do you want to print other steps? Enter 'y' for yes and 'n' for no.\n")
-    if choice == 'n':
+    #### Continue or not?
+    if printStep == 'q':
         stop = True
 
-print('End of the examples.')
+print('You quit.')
